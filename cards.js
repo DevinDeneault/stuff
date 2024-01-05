298353aed21720090f1aa069115456183ef3b863
Devin Deneault <devdeneault@gmail.com>
2024-01-05 04:22:43 -0700

    fixed length call, it's not a function

--- cards.js
+++ cards.js
@@ -3,7 +3,7 @@ import { occultDeck } from './data/occult.js';
 import { arcaneDeck } from './data/arcane.js';
 import { divineDeck } from './data/divine.js';
 
-console.log(primalDeck.length());
-console.log(occultDeck.length());
-console.log(arcaneDeck.length());
-console.log(divineDeck.length());
+console.log(primalDeck.length);
+console.log(occultDeck.length);
+console.log(arcaneDeck.length);
+console.log(divineDeck.length);
