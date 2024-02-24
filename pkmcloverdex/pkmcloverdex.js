88ffd8dc23e9d864f89a3d3935d8b8cdb20248b1
- <->
2024-02-24 05:53:53 -0700

    stuff

--- pkmcloverdex/pkmcloverdex.js
+++ pkmcloverdex/pkmcloverdex.js
@@ -3,24 +3,28 @@ let showingShinySprites = false;
 let images = [];
 
 document.addEventListener("DOMContentLoaded", () => {
-  images = document.querySelectorAll("img");
+  images = document.querySelectorAll(".dex-entry > div:nth-child(2) > div");
+
+  for (let i = 0; i < images.length; i++) {
+    const x = (i % 21) * 64;
+    const y = (Math.floor(i / 21)) *64;
+    const offset = `-${x}px -${y}px`;
+
+    images[i].style.backgroundPosition = offset;
+  }
 });
 
 function toggleShiny(e) {
   if (showingShinySprites) {
     e.textContent = "See shiny sprites";
     for (let i = 0; i < images.length - 1; i++) {
-      const srcSplit = images[i].src.split("/");
-      const fileName = srcSplit[srcSplit.length - 1];
-      images[i].src = `images/sprites-normal/${fileName}`;
+      images[i].style.backgroundImage = "url('images/clover-sprites.png')";
     }
   }
   else {
     e.textContent = "See non-shiny sprites";
     for (let i = 0; i < images.length - 1; i++) {
-      const srcSplit = images[i].src.split("/");
-      const fileName = srcSplit[srcSplit.length - 1];
-      images[i].src = `images/sprites-shiny/${fileName}`;
+      images[i].style.backgroundImage = "url('images/clover-sprites-shiny.png')";
     }
   }
 
