96d5c62dc6fe5e4747b34b489a7363b3a43c698d
- <->
2024-02-28 18:55:32 -0700

    stuff

--- doomfire/doomfire.js
+++ doomfire/doomfire.js
@@ -1,72 +1,60 @@
 
-// MODIFIED FROM SOURCE:
 // https://fabiensanglard.net/doom_fire_psx/
 
 const CJS_TICKER_FPS = 27;
 const FIRE_WIDTH = 320;
-const FIRE_HEIGHT = 168
-
-let palette = [];
+const FIRE_HEIGHT = 168;
 
 // Palette based framebuffer. Coordinate system origin upper-left.
 let firePixels = [];
 
-const RGBS = [
-  0x07,0x07,0x07,
-  0x1F,0x07,0x07,
-  0x2F,0x0F,0x07,
-  0x47,0x0F,0x07,
-  0x57,0x17,0x07,
-  0x67,0x1F,0x07,
-  0x77,0x1F,0x07,
-  0x8F,0x27,0x07,
-  0x9F,0x2F,0x07,
-  0xAF,0x3F,0x07,
-  0xBF,0x47,0x07,
-  0xC7,0x47,0x07,
-  0xDF,0x4F,0x07,
-  0xDF,0x57,0x07,
-  0xDF,0x57,0x07,
-  0xD7,0x5F,0x07,
-  0xD7,0x5F,0x07,
-  0xD7,0x67,0x0F,
-  0xCF,0x6F,0x0F,
-  0xCF,0x77,0x0F,
-  0xCF,0x7F,0x0F,
-  0xCF,0x87,0x17,
-  0xC7,0x87,0x17,
-  0xC7,0x8F,0x17,
-  0xC7,0x97,0x1F,
-  0xBF,0x9F,0x1F,
-  0xBF,0x9F,0x1F,
-  0xBF,0xA7,0x27,
-  0xBF,0xA7,0x27,
-  0xBF,0xAF,0x2F,
-  0xB7,0xAF,0x2F,
-  0xB7,0xB7,0x2F,
-  0xB7,0xB7,0x37,
-  0xCF,0xCF,0x6F,
-  0xDF,0xDF,0x9F,
-  0xEF,0xEF,0xC7,
-  0xFF,0xFF,0xFF
+const PALETTE = [
+  { r: 7, g: 7, b: 7 },
+  { r: 31, g: 7, b: 7 },
+  { r: 47, g: 15, b: 7 },
+  { r: 71, g: 15, b: 7 },
+  { r: 87, g: 23, b: 7 },
+  { r: 103, g: 31, b: 7 },
+  { r: 119, g: 31, b: 7 },
+  { r: 143, g: 39, b: 7 },
+  { r: 159, g: 47, b: 7 },
+  { r: 175, g: 63, b: 7 },
+  { r: 191, g: 71, b: 7 },
+  { r: 199, g: 71, b: 7 },
+  { r: 223, g: 79, b: 7 },
+  { r: 223, g: 87, b: 7 },
+  { r: 223, g: 87, b: 7 },
+  { r: 215, g: 95, b: 7 },
+  { r: 215, g: 95, b: 7 },
+  { r: 215, g: 103, b: 15 },
+  { r: 207, g: 111, b: 15 },
+  { r: 207, g: 119, b: 15 },
+  { r: 207, g: 127, b: 15 },
+  { r: 207, g: 135, b: 23 },
+  { r: 199, g: 135, b: 23 },
+  { r: 199, g: 143, b: 23 },
+  { r: 199, g: 151, b: 31 },
+  { r: 191, g: 159, b: 31 },
+  { r: 191, g: 159, b: 31 },
+  { r: 191, g: 167, b: 39 },
+  { r: 191, g: 167, b: 39 },
+  { r: 191, g: 175, b: 47 },
+  { r: 183, g: 175, b: 47 },
+  { r: 183, g: 183, b: 47 },
+  { r: 183, g: 183, b: 55 },
+  { r: 207, g: 207, b: 111 },
+  { r: 223, g: 223, b: 159 },
+  { r: 239, g: 239, b: 199 },
+  { r: 255, g: 255, b: 255 }
 ];
 
-let y_scrolling = 540;
+let y_scrolling = 440;
 let stage;
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Populate pallete.
-  for(var i = 0; i < RGBS.length / 3; i++) {
-    palette[i] = {
-      r : RGBS[i * 3 + 0],
-      g : RGBS[i * 3 + 1],
-      b : RGBS[i * 3 + 2]
-    }
-  }
-
   const paletteDivs = document.querySelectorAll("#doom-color-column > div");
   for (let x = 0; x < paletteDivs.length; x++) {
-    paletteDivs[x].style.backgroundColor = `rgb(${palette[x].r}, ${palette[x].g}, ${palette[x].b})`;
+    paletteDivs[x].style.backgroundColor = `rgb(${PALETTE[x].r}, ${PALETTE[x].g}, ${PALETTE[x].b})`;
   }
 
   stage = new createjs.Stage("backbuffer");
@@ -75,16 +63,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const container = new createjs.Container();
   stage.addChild(container);
 
-  // Set whole screen to 0 (color: 0x07,0x07,0x07)
-  for (let i = 0; i < FIRE_WIDTH*FIRE_HEIGHT; i++) {
-    firePixels[i] = 0;
-  }
-  
-  // Set bottom line to 36 (color white: 0xFF,0xFF,0xFF)
-  for (let i = 0; i < FIRE_WIDTH; i++) {
-    firePixels[(FIRE_HEIGHT - 1) * FIRE_WIDTH + i] = 36;
-  }
-  y_scrolling = 440;
+  // Set whole screen to color 0
+  for (let i = 0; i < FIRE_WIDTH * FIRE_HEIGHT; i++) { firePixels[i] = 0; }
+  // Set bottom line to color 36
+  for (let i = 0; i < FIRE_WIDTH; i++) { firePixels[(FIRE_HEIGHT - 1) * FIRE_WIDTH + i] = 36; }
 });
 
 // ============================================================================
@@ -125,7 +107,7 @@ function hostFrame() {
   for (let y = 0; y < canvas.height; y++) {
     for (let x = 0; x < canvas.width; x++) {
       const index = firePixels[y * canvas.width + x];
-      const pixel = palette[index];
+      const pixel = PALETTE[index];
 
       color.data[((canvas.width * y) + x) * 4 + 0] = pixel.r;
       color.data[((canvas.width * y) + x) * 4 + 1] = pixel.g;
