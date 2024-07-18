e9321d9b36329a8344fb5bae3504a695f82f6a7c
- <->
2024-07-18 13:27:48 -0600

    stuff

--- globals.js
+++ globals.js
@@ -1,2 +1,9 @@
 
 function gebi(name) { return document.getElementById(name); };
+
+document.addEventListener('keydown', event => {
+  if (event.ctrlKey && event.key === 'h') {
+      event.preventDefault();
+      window.location.href = '/';
+  }
+});
