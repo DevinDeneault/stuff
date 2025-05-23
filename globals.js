a8f6182803c055b0f49f2d5797f16a7ff6f7a656
devdeneault <devdeneault@gmail.com>
2025-05-23 13:02:00 -0600

    stuff

--- globals.js
+++ globals.js
@@ -8,7 +8,7 @@ window.addEventListener('load', function() {
       <path d="M8.543 2.232a.75.75 0 0 0-1.085 0l-5.25 5.5A.75.75 0 0 0 2.75 9H4v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 1 1 2 0v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V9h1.25a.75.75 0 0 0 .543-1.268l-5.25-5.5Z" />
     </svg>
   `;
-  homeButton.style = 'position:fixed; right:0; bottom:0; height:2rem; width:2rem; background-color:rgba(255,255,255,0.4); z-index:1000; cursor:pointer; border-radius:0.5rem 0.2rem 0 0.2rem; display:flex; align-items:center; justify-content:center;';
+  homeButton.style = 'position:fixed; right:0; bottom:0; height:2.5vh; width:2.5vh; background-color:rgba(255,255,255,0.4); z-index:1000; cursor:pointer; border-radius:30% 10% 0 10%; display:flex; align-items:center; justify-content:center;';
   homeButton.onclick = () => { window.location.href = '/stuff'; };
   document.body.appendChild(homeButton);
 });
