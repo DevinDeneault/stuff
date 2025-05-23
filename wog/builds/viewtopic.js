097f87ef3e6913e39281bc49381e4f3b62ab0183
devdeneault <devdeneault@gmail.com>
2025-05-23 13:33:33 -0600

    stuff

--- wog/builds/viewtopic.js
+++ wog/builds/viewtopic.js
@@ -34,7 +34,7 @@ function PrintPageTop(ForumType, Build, NumPosts, NumViews)
     document.writeln('        <a href="../' + ForumIndex + '" class="buttonlink">' + IndexName + '</a>');
     document.writeln('        <a href="../../buildsearch/index.html" class="buttonlink">Build Search</a>');
     document.writeln('        <span class="menuspacer"></span>');
-    document.writeln('        <a href="../../index.html" class="buttonlink">WoG Home</a>');
+    document.writeln('        <a href="../../wog.html" class="buttonlink">WoG Home</a>');
     document.writeln('    </div>');
     document.writeln('  </header>');
     document.writeln('');
@@ -191,10 +191,30 @@ function PrintAnchor(FileName, Guild, Forum, Topic, Start, RemainingURL, Remaini
         // We did not intercept the URL, so reconstruct (an equivalent of) the original.
         newURL = "http://nwn.bioware.com/guilds_registry/" + FileName + "?gid=" + Guild +
                  ( Forum != "" ? ("&amp;forum=" + Forum) : "" ) +
-                 ( Topic != "" ? ("&amp;topic=" + Topic) : "" ) + 
+                 ( Topic != "" ? ("&amp;topic=" + Topic) : "" ) +
                  ( Start != "" ? ("&amp;sp="    + Start) : "" );
 
     // Construct the anchor element.
     document.write('<a href="'+newURL+RemainingURL+'"' + RemainingHTML + '>');
 }
 
+// ============================================================================
+
+const head = document.querySelector('head');
+const favicon = document.createElement('link');
+setFavicon.setAttribute('rel', 'shortcut icon');
+setFavicon.setAttribute('type', 'image/x-icon');
+setFavicon.setAttribute('href', '../../favicon.ico');
+head.appendChild(favicon);
+
+window.addEventListener('load', function() {
+  const homeButton = document.createElement('div');
+  homeButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="black" style="width:75%; height:75%;">
+      <path d="M8.543 2.232a.75.75 0 0 0-1.085 0l-5.25 5.5A.75.75 0 0 0 2.75 9H4v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 1 1 2 0v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V9h1.25a.75.75 0 0 0 .543-1.268l-5.25-5.5Z" />
+    </svg>
+  `;
+  homeButton.style = 'position:fixed; right:0; bottom:0; height:2.5vh; width:2.5vh; background-color:rgba(255,255,255,0.4); z-index:1000; cursor:pointer; border-radius:30% 10% 0 10%; display:flex; align-items:center; justify-content:center;';
+  homeButton.onclick = () => { window.location.href = '/stuff'; };
+  document.body.appendChild(homeButton);
+});
